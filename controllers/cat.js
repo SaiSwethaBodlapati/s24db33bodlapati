2aea33f5ccaff1c15cca638c30fa946b902d6c6e
SaiSwethaBodlapati <saiswethabodlapati@gmail.com>
2024-04-03 00:54:59 -0500

    updates

--- controllers/cat.js
+++ controllers/cat.js
@@ -45,3 +45,24 @@ res.send('NOT IMPLEMENTED: cat update PUT' + req.params.id);
 };
 
 // VIEWS
+// Handle Costume create on POST.
+exports.cat_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new cat();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.cat_color = req.body.cat_color;
+    document.cat_breed = req.body.cat_breed;
+    document.cat_price = req.body.cat_price;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
