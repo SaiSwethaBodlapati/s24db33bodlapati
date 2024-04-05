985226891ab698d21bfa84163366bb073f84370d
SaiSwethaBodlapati <saiswethabodlapati@gmail.com>
2024-04-04 21:57:21 -0500

    update

--- controllers/cat.js
+++ controllers/cat.js
@@ -129,3 +129,16 @@ exports.cat_view_one_Page = async function(req, res) {
     }
     };
     
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.costume_update_Page = async function(req, res) {
+console.log("update view for item "+req.query.id)
+try{
+let result = await cat.findById(req.query.id)
+res.render('catupdate', { title: 'Cat Update', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
--- routes/cat.js
+++ routes/cat.js
@@ -16,4 +16,9 @@ router.get('/', costume_controller.cat_view_all_Page );
 router.get('/detail', costume_controller.cat_view_one_Page);
 /* GET create costume page */
 router.get('/create', costume_controller.cat_create_Page);
+/* GET create update page */
+router.get('/update', costume_controller.cat_update_Page);
+
+
 module.exports = router;
+
