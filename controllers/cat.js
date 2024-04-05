25f611c37888010d5c709f40c50201e3c2547821
SaiSwethaBodlapati <saiswethabodlapati@gmail.com>
2024-04-04 20:42:48 -0500

    update

--- controllers/cat.js
+++ controllers/cat.js
@@ -28,9 +28,18 @@ exports.cat_view_all_Page = async function(req, res) {
 // res.send('NOT IMPLEMENTED: cat list');
 // };
 // for a specific Costume.
-exports.cat_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: cat detail: ' + req.params.id);
+// for a specific Costume.
+exports.cat_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await cat.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
 };
+
 // Handle Costume create on POST.
 // exports.cat_create_post = function(req, res) {
 // res.send('NOT IMPLEMENTED: cat create POST');
@@ -40,10 +49,26 @@ exports.cat_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: cat delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.cat_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: cat update PUT' + req.params.id);
-    };
-
+//Handle Costume update form on PUT.
+exports.cat_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await cat.findById( req.params.id)
+// Do updates of properties
+if(req.body.cat_color)
+toUpdate.cat_color = req.body.cat_color;
+if(req.body.cat_breed) toUpdate.cost = req.body.cat_breed;
+if(req.body.cat_price) toUpdate.size = req.body.cat_price;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
 
 
 // VIEWS
