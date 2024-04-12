c8e35b7e0774371956fae3b5913cce3e1195c463
SaiSwethaBodlapati <saiswethabodlapati@gmail.com>
2024-04-11 19:22:34 -0500

    Update

--- models/cat.js
+++ models/cat.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose")
 const costumeSchema = mongoose.Schema({
 cat_color: String,
 cat_breed: {type:String,lenght:15},
-cat_price:{type:Number,min:1000,max:200000}
+cat_price:{type:Number,min:1000,max:20000}
 })
 module.exports = mongoose.model("cat",costumeSchema)
--- routes/cat.js
+++ routes/cat.js
@@ -9,18 +9,26 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 var costume_controller = require('../controllers/cat');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
 
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 router.get('/', costume_controller.cat_view_all_Page );
 
 /* GET detail costume page */
-router.get('/detail', costume_controller.cat_view_one_Page);
+router.get('/detail',secured, costume_controller.cat_view_one_Page);
 /* GET create costume page */
-router.get('/create', costume_controller.cat_create_Page);
+router.get('/create',secured, costume_controller.cat_create_Page);
 /* GET create update page */
-router.get('/update', costume_controller.cat_update_Page);
+router.get('/update', secured,costume_controller.cat_update_Page);
 
 /* GET delete cat page */
-router.get('/delete',costume_controller.cat_delete_Page);
+router.get('/delete',secured,costume_controller.cat_delete_Page);
 
 module.exports = router;
 
